0982a287c60cae85156de74867bb64f583908025
mailalertssanjaydutt-tech <mailalerts.sanjaydutt@gmail.com>
2025-12-31 15:57:59 +0530

    Update Footer.jsx

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -52,7 +52,7 @@ const Footer = () => {
     </div>
   </section>
 
-        <div class="support-icon"><a href="https://wa.me/8572063223"><img src="images/support-icon1.png" alt="support icon" /></a></div>
+        <div class="support-icon"><a href="https://wa.me/8572063223"><img src="/images/support-icon1.png" alt="support icon" /></a></div>
 </>
 
     </div>
